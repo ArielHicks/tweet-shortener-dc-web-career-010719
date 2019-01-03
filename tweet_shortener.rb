eb2ec579c6d1a3345146ba370523317299d8aed3
Ariel Hicks <hicks.ariel@gmail.com>
2019-01-03 18:06:45 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,3 +1,5 @@
+phrase = "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
+
 require "pry"
 
 def dictionary
